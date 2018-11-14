8f90d272e207aeaf5db6976c46b642999b14e182
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-11-14 22:07:44 +0500

    update from class room

--- Ch14-P2-CarLibrary/Properties/AssemblyInfo.cs
+++ Ch14-P2-CarLibrary/Properties/AssemblyInfo.cs
@@ -37,4 +37,5 @@ using System.Runtime.InteropServices;
 //[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-//[assembly: AssemblyKeyFile(@"C:\MyTestKeyPair\MyTestKeyPair.snk")]
+
+[assembly: AssemblyKeyFile(@"C:\MyTestKeyPair\MyTestKeyPair.snk")]
