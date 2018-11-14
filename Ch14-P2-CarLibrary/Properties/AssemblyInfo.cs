5787b342c6afa5679c9b03f0c6c3fa96fe38876b
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-14 16:57:21 +0500

    Added code to make a shared library for GAC

--- Ch14-P2-CarLibrary/Properties/AssemblyInfo.cs
+++ Ch14-P2-CarLibrary/Properties/AssemblyInfo.cs
@@ -32,5 +32,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
+// Valid values for each part of version number are between 0 and 65535
+//[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyKeyFile(@"C:\MyTestKeyPair\MyTestKeyPair.snk")]
